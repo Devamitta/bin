fba118e66056e60539e79dc9cd717d52511960d7
Devamitta <devamitta@sasanarakkha.org>
2023-12-22 21:50:27 +0800

    updates

--- db_from_backup.sh
+++ db_from_backup.sh
@@ -4,7 +4,7 @@ echo -e "\033[1;33m We are going to rebuild db from backup \033[0m"
 
 cd '/home/deva/Documents/dpd-db'
 
-poetry run python scripts/db_rebuild_from_tsv.py
+poetry run bash dps/bash/rebuild_db.sh
 
 
 
--- mkdpd.sh
+++ mkdpd.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-echo -e "\033[1;33m We are going to make DPD dictionary \033[0m"
+echo -e "\033[1;33m We are going to make DPD dictionary Started at $(date) \033[0m"
 
 
 OUTPUT_FILE="/home/deva/logs/ram_swap_usage_mkdpd.tsv"
+
+echo -e "We are going to monitor usage of RAM in the background, see log folder"
 
 # Start the RAM and SWAP monitoring in the background
 {
@@ -25,12 +27,15 @@ MONITOR_PID=$!
 
 cd "/home/deva/Documents/dpd-db/"
 
-poetry run bash dps/bash/make_dpd.sh
+# Start capturing all terminal output and save to a log file
+script -f -c "poetry run bash dps/bash/make_dpd.sh" "/home/deva/logs/mkdpd.log"
+
+echo "------ generate DPD for GoldenDict Finished at $(date) ------"
 
 # Kill the RAM usage monitoring process once your script is done.
 kill $MONITOR_PID
 
-# open log.
+# open log
 # xed '/home/deva/logs/mkdpd.log'
 
 # Check if GoldenDict is running
